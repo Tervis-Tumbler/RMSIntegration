d96d264ef77ad0200c4153af703b244ac5fc25ad
ChrisMagnuson <hourglasssand@gmail.com>
2016-11-15 14:44:58 -0500

    Move Start-ParallelWork to TervisPowerShellJobs

--- TervisRMS.psm1
+++ TervisRMS.psm1
@@ -1,33 +1,4 @@
-﻿function Start-ParallelWork {
-    param (
-        $ScriptBlock,
-        $Parameters
-    )
-    $Jobs = @()
-
-    foreach ($Parameter in $Parameters) {
-        while ($(Get-Job -State Running | where Id -In $Jobs.Id | Measure).count -ge 10) { Start-Sleep -Milliseconds 100 }
-        $Jobs += Start-Job -ScriptBlock $ScriptBlock -ArgumentList $Parameter
-    }
-
-    while (
-        Get-Job -State Running | 
-        where Id -In $Jobs.Id
-    ) {
-        Write-Verbose "Sleeping for 100 milliseconds"
-        Start-Sleep -Milliseconds 100 
-    }
-    
-    $Results = Get-Job -HasMoreData $true | 
-    where Id -In $Jobs.Id |
-    Receive-Job
-
-    Get-Job -State Completed | 
-    where Id -In $Jobs.Id | 
-    Remove-Job
-    
-    $Results
-}
+﻿#Requires -modules TervisPowerShellJobs
 
 function Get-BackOfficeComputers {
     param(
